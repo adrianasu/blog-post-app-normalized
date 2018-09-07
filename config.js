8c5340a20eef211556c36a71cf1b31ba05fee3e9
Adriana Suarez <40244603+adrianasu@users.noreply.github.com>
2018-09-07 14:48:08 -0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,4 @@ exports.TEST_DATABASE_URL =
 exports.PORT = process.env.PORT || 8080;
 
 
-//TEST_DATABASE_URL=mongodb://adrianasu:8ur8ujitaM@ds141942.mlab.com:41942/test-blog-posts npm test
-
-//DATABASE_URL=mongodb://adrianasu:8ur8ujitaM@ds239692.mlab.com:39692/blog-post-app npm start
 
